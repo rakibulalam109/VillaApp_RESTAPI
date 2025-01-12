127a04452b345551aa6b616add68aa85102011a6
rakibul alam <rakibulalam109@gmail.com>
2025-01-12 12:15:01 +0600

    Add project files.

---
+++ Villa_API/Controllers/VillaAPIController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Villa_API.Models;
+
+namespace Villa_API.Controllers
+{
+    [Route("api/VillaAPI")]
+    [ApiController]
+    public class VillaAPIController:ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Villa> Get()
+        {
+            return new List<Villa>
+            {
+                new Villa{Id=1,Name="Pool View"},
+                new Villa{Id=2,Name="Beach View"}
+            };
+        }
+    }
+}
---
+++ Villa_API/Models/Villa.cs
@@ -0,0 +1,8 @@
+﻿namespace Villa_API.Models
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
