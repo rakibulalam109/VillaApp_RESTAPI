932b11e22f2fb1407531d41919701efeee2cc506
rakibul alam <rakibulalam109@gmail.com>
2025-01-13 12:30:36 +0600

    Added Data Annotation, Custom Validation and Model State

--- Villa_API/Controllers/VillaAPIController.cs
+++ Villa_API/Controllers/VillaAPIController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Villa_API.Data;
 using Villa_API.Models;
 using Villa_API.Models.DTOs;
 
@@ -6,16 +7,56 @@ namespace Villa_API.Controllers
 {
     [Route("api/VillaAPI")]
     [ApiController]
-    public class VillaAPIController:ControllerBase
+    public class VillaAPIController : ControllerBase
     {
         [HttpGet]
-        public IEnumerable<VillaDTO> Get()
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
         {
-            return new List<VillaDTO>
+            return Ok(VillaStorage.villaList);
+        }
+
+        [HttpGet("{id:int}",Name = "GetVilla")]
+        public ActionResult<VillaDTO> GetVilla(int id)
+        {
+            if (id == 0)
             {
-                new VillaDTO{Id=1,Name="Pool View"},
-                new VillaDTO{Id=2,Name="Beach View"}
-            };
+                return BadRequest();
+            }
+            var villa = Ok(VillaStorage.villaList.FirstOrDefault(u => u.Id == id));
+
+            if (villa == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(villa);
+        }
+
+        [HttpPost]
+        public ActionResult<VillaDTO> AddVilla([FromBody] VillaDTO addVilla)
+        {
+            if (VillaStorage.villaList.FirstOrDefault(u=>u.Name.ToLower() == addVilla.Name.ToLower()) != null) 
+            {
+                ModelState.AddModelError("","Villa Already Exists");
+                return BadRequest(ModelState);
+            }
+
+            if (addVilla == null)
+            {
+                return BadRequest(addVilla);
+            }
+            if (addVilla.Id > 0)
+            {
+                return StatusCode(StatusCodes.Status505HttpVersionNotsupported);
+            }
+
+            addVilla.Id = VillaStorage.villaList.OrderByDescending(u => u.Id).FirstOrDefault().Id + 1;
+
+            VillaStorage.villaList.Add(addVilla);
+
+            //return Ok(addVilla);
+            return CreatedAtRoute("GetVilla", new { id = addVilla.Id }, addVilla);
+
         }
     }
 }
---
+++ Villa_API/Data/VillaStorage.cs
@@ -0,0 +1,13 @@
+﻿using Villa_API.Models.DTOs;
+
+namespace Villa_API.Data
+{
+    public class VillaStorage
+    {
+        public static List<VillaDTO> villaList = new List<VillaDTO>
+        {
+            new VillaDTO{Id=1,Name="Pool View"},
+            new VillaDTO{Id=2,Name="Lake View"}
+        };
+    }
+}
